e13b41f727cc4b32ad872079d25ccd01fb0c70c0
tosuke <mail@tosuke.me>
2022-12-17 21:58:21 +0900

    feat: template listing for BGP peers

--- ct_config.tf
+++ ct_config.tf
@@ -12,7 +12,7 @@ data "ct_config" "nodes" {
       primary_ipv4 = each.value.primary_ipv4,
       primary_ipv6 = each.value.primary_ipv6,
 
-      dns = "192.168.20.1", # FIXME: correct DNS server
+      dns = "192.168.20.100", # FIXME: correct DNS server
 
       eth0_mac = each.value.mac,
       eth0_ipv4 = each.value.eth0_ipv4,
@@ -21,12 +21,19 @@ data "ct_config" "nodes" {
       bird_img = "${var.matchbox_http_endpoint}/assets/bird/bird-${each.value.arch}.tar.gz",
 
       local_asn = each.value.asn,
-      peer1 = "192.168.20.100",
-      peer1_asn = 65100,
-      peer1_nexthop_ipv6 = each.value.eth0_ipv6,
-      peer2 = "192.168.20.101",
-      peer2_asn = 65100,
-      peer2_nexthop_ipv6 = each.value.eth0_ipv6,
+
+      peers = [
+        {
+          asn = 65100,
+          address = "192.168.20.100",
+          nexthop_ipv6 = each.value.eth0_ipv6,
+        },
+        {
+          asn = 65100,
+          address = "192.168.20.101",
+          nexthop_ipv6 = each.value.eth0_ipv6,
+        },
+      ],
     }),
   ]
 }
